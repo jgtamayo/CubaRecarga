badcf5cd1b3023f8acb507944c91fe15bb2bcbfb
jorge <jorgetamayohernandez@gmail.com>
2017-04-21 17:23:41 -0400

    First commit

---
+++ www/js/controllers/IndexCtrl.js
@@ -0,0 +1,39 @@
+﻿app.controller('IndexCtrl', function ($scope, $stateParams, ionicMaterialMotion) {
+
+
+    /*
+    $scope.ripple = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-ripple';
+        setTimeout(function() {
+            ionicMaterialMotion.ripple();
+        }, 500);
+    };
+
+    $scope.fadeSlideInRight = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in-right';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideInRight();
+        }, 500);
+    };
+
+    $scope.fadeSlideIn = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideIn();
+        }, 500);
+    };
+
+    $scope.blinds = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-blinds';
+        setTimeout(function() {
+            ionicMaterialMotion.blinds(); // ionic.material.motion.blinds(); //ionicMaterialMotion
+        }, 500);
+    };
+
+    $scope.blinds();*/
+
+});
